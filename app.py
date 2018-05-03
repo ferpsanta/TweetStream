c34305d07d755db61f04a44a2c1618eef1def1e0
fsantamarina <f.perezsanta@gmail.com>
2018-05-03 09:59:35 +0200

    App base and config loading.

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__, template_folder="templates")
+socketio = SocketIO(app, async_mode=None)  # Async mode will be handled automatically
+
+# Load configuration from config.cfg
+app.config.from_pyfile('config.cfg')
+# Push the app configuration to the app context for it to be available to the submodules.
+app.app_context().push()
+
 if __name__ == "__main__":
-    pass
+    app.debug = True
+    socketio.run(app)
